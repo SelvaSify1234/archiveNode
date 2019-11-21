0200805adec48b125f26c84feb2da9266baf4a24
Selva kumar P <palani.selvakumar@sifycorp.com>
2019-11-21 15:07:36 +0530

    all code cleanup completed

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 var fs = require('fs');
 const bodyParser = require("body-parser");
 var mysql = require('mysql');
@@ -28,180 +27,183 @@ var sel_query;
 var del_query;
 var sour_table;
 var dest_table;
-var sequence;
-var post=0;
+var sequence = 0;
+var post = 0;
+global.msg = '';
+let flg_sor = 0;
 
 var app = express();
 app.use(bodyParser.json());
 app.use(helmet());
 app.use(cors());
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
 
 /* Archive Data Post Call*/
-  app.post('/db/archive-manual',function(req,res,next)
-  {
+app.post('/db/archive-manual', function(req, res, next) {
     console.log('Testing');
     log.create_log();
 
-   dbkeys =Object.keys(req.body.database_config);
+    dbkeys = Object.keys(req.body.database_config);
 
-   for (var key in req.body['database_config'][dbkeys]) 
-      {
-       eval(key+ " = req.body['database_config'][dbkeys][key]");
-      }
+    for (var key in req.body['database_config'][dbkeys]) {
+        eval(key + " = req.body['database_config'][dbkeys][key]");
+    }
 
-  sour_con = mysql.createConnection({ host:mysql_source_host,  user: mysql_source_username,  password:mysql_source_password,database: mysql_source_database});
-  dest_con = mysql.createConnection({host: mysql_destination_host,user: mysql_destination_username, password:mysql_destination_password, database: mysql_destination_database  });  
-   customer_type = customer_type;
-   sour_host = mysql_source_host;
-   dest_host=mysql_destination_host;
-   sour_db=mysql_source_database;
-   dest_db=mysql_destination_database;
-   sour_port= mysql_source_port;
-   dest_port=mysql_destination_port;
-  create_table=create_dest_table_if_not_exists;
-  module_name=mysql_select_module;
-  
-   sour_con.connect(function(err) {
-       if (err !=null) 
-       {log.info('Source database not connected !.. Please make sure source database configuration valid.');
-         response('Source database not connected !.. Please make sure source database configuration valid.');}
-       else
-       {  log.info('Source database connected successfully.');    }
-    });
-    
-
-    dest_con.connect(function(err) {
-        if (err !=null) 
-        {log.error('Destination database not connected !.. Please make sure destination Details  ');
-        response('Destination database not connected !.. Please Make sure destination Details  ');}
-        else
-        {          log.info('Destination database connected successfully.');        }
-    });
-    /* Archive Data For FourmNXT Type */ 
+    sour_con = mysql.createConnection({ host: mysql_source_host, user: mysql_source_username, password: mysql_source_password, database: mysql_source_database });
+    dest_con = mysql.createConnection({ host: mysql_destination_host, user: mysql_destination_username, password: mysql_destination_password, database: mysql_destination_database });
+    customer_type = customer_type;
+    sour_host = mysql_source_host;
+    dest_host = mysql_destination_host;
+    sour_db = mysql_source_database;
+    dest_db = mysql_destination_database;
+    sour_port = mysql_source_port;
+    dest_port = mysql_destination_port;
+    create_table = create_dest_table_if_not_exists;
+    module_name = mysql_select_module;
 
 
-    if(customer_type=='1' )
-    {
+    source_conn(sour_con, dest_con)
+        .then(rst => {
+            
+            if (rst) {
 
-    get_sales_data(sour_con,module_name,sour_db).then(result =>{
-       
-    if(result.length>0)
-     {
-      forEach(result, function(item, index, arr) 
-        {
-            sel_query =  item['sel_query'];
-            del_query =  item['del_query'];
-            sequence  =  item['vt_tabid'];
-            sour_table =   sel_query.match(new RegExp('FROM' + "(.*)" + 'WHERE'))[1];
-            sour_table = sour_table.substring(0,sour_table.indexOf("WHERE"));
-            sour_table = sour_table.replace(/\s/g, "");
-            dest_table = sour_table+'_archival';
-           
-            /* Do Archive */ 
-            archive.do_archive(sour_con,dest_con,create_table,module_name,sour_db,dest_db,sour_host,dest_host,sour_port,dest_port,sour_table,dest_table,sel_query,del_query,sequence).then(
-             stat => {
-               if(stat){
-                log.info('Data Archive has been done.');
-                       //if(index== result.length-1)
-                       //{ response ('Data Archive has been done.'); }
-                    response (' Data archive process has been completed.');
-               }
-               else{
-               // response ('Some Reasone Data Archive has been NOT Done.');
-               log.info('Some reasone data archive has been NOT Done.');
-              //  res.setHeader('Content-Type', 'application/json');
-              //  res.send({message: 'Data Archive has been done.' });
-              //  res.end;
-          }
+                if (customer_type == '1' && sour_con.state == 'authenticated' && dest_con.state == 'authenticated') 
+                {
+                    get_sales_data(sour_con, module_name, sour_db)
+                        .then(result => {
+                            if (result.length > 0) 
+                            {
+                                forEach(result, function(item, index, arr) {
+                                    sel_query = item['sel_query'];
+                                    del_query = item['del_query'];
+                                    sequence = item['vt_tabid'];
+                                    sour_table = sel_query.match(new RegExp('FROM' + "(.*)" + 'WHERE'))[1];
+                                    sour_table = sour_table.substring(0, sour_table.indexOf("WHERE"));
+                                    sour_table = sour_table.replace(/\s/g, "");
+                                    dest_table = sour_table + '_archival';
+
+                                    /* Do Archive */
+                                    archive.do_archive(sour_con, dest_con, create_table, module_name, sour_db, dest_db, sour_host, dest_host, sour_port, dest_port, sour_table, dest_table, sel_query, del_query, sequence)
+                                        .then(stat => {
+                                            if (stat) 
+                                            {
+                                                log.info('Data Archive has been done.');                                                
+                                                msg = ' Data archive process has been completed.';
+                                                // return;
+
+                                            } else {
+                                                log.info('Some reasone data archive has been NOT Done.');
+                                                msg = 'Some reasone data archive has been NOT Done.';
+                                            }
+                                        })
+                                        .catch(err => {
+                                            console.log('Do Archive');
+                                            log.error('\n----------------------\n');
+                                            msg = err.message;
+                                            log.error(err.message);
+                                            log.error('\n----------------------\n');
+                                            
+                                            console.log('Do Archive Msg :', msg);
+                                            log.log_entry(sour_con, sequence, module_name, '2', sour_db);
+
+                                        });
+                                });
+                            }
+                        })
+                        .catch(err => {
+                            post = 1;
+                            console.log('Get data');
+                            log.error('\n----------------------\n');
+                            log.error(err.message);
+                            msg = err.message;
+                            log.error('\n----------------------\n');
+                            console.log('Get data catch Msg :', msg);
+                            
+                            if (sequence != 0)
+                                log.log_entry(sour_con, sequence, module_name, '2', sour_db);
+
+                        })
+                        .finally(_ => {
+                          console.log('Get data finally');
+                            if (msg == '') { msg = 'Data archive process has been completed.'; }
+                            res.setHeader('Content-Type', 'application/json');
+                            res.send({ message: msg });
+                            res.end();
+                        });
+
+                } else { /* Archive Data For Others Type */ }
+            }
         })
-      .catch(err=>{
-       
-         log.error('\n----------------------\n');
-         log.error(err.message);
-         log.error('\n----------------------\n');
-         post=1;
-          response (err.message);
-          console.log(post);       
-         log.log_entry(sour_con,sequence,module_name,'2',sour_db);
-         
-        })
-   }); 
-}
+        .catch(err => {
+            console.log('Do Archive1 catch msg:');
+            log.error('\n----------------------\n');
+            log.error(err.message);
+            msg=err.message;
+            log.error('\n----------------------\n');
+            //post=0;
+            res.setHeader('Content-Type', 'application/json');
+            res.send({ message: msg });
+            res.end();
+            msg='';
 
-})
-.catch(err=>{
-  post=1;
+        });
 
- log.error('\n----------------------\n');
- log.error(err.message);
- log.error('\n----------------------\n');
- response (err.message); 
- console.log(post); 
- log.log_entry(sour_con,sequence,module_name,'2',sour_db);
- 
-})
-.finally( _ => 
-  {
- //  console.log(post);  
- //   if(post==0)
- //   {
-  //    console.log('finally');
-  //    console.log(post);
-  //    response (' Data archive process has been completed.');
-  //    posts=0;
-  //  }
-   
-     // sour_con.end();
-    //dest_con.end();
+    // function response (message)
+    //  {
+    //   res.setHeader('Content-Type', 'application/json');
+    //   res.send({message: message });
+    //   res.end();
+    //  }   
+
 });
-   
- }
-
- 
-    else{          /* Archive Data For Others Type */     }
 
 
-   
-
-function response (message)
-{
-  res.setHeader('Content-Type', 'application/json');
-  res.send({message: message });
-  res.end;
-}   
- });
-
-
-async function get_sales_data(sour_con,name,sour_db)
-{
-    return new Promise((rs,rj)=>{
-   var sql  ='select * from sify_darc_modules_query where module_name="'+ name  +'"  order by sequence asc';
-   sour_con.query(sql, function(err,result,fields){
-   if(err || !result)
-    { rj(new Error('Source database select table error ','some service ',+ err.message));
-      return false;
-    }   
-    else{
-        if(result.length>0)
-           rs(result);
-       else{
-             rj(new Error('Module Name is not valide. Module Name data not exist. '));
-             log.info('Module Name is not valide. Module Name data not exist');
-              return false;
-          }        
-      }      
+async function get_sales_data(sour_con, name, sour_db) {
+    return new Promise((rs, rj) => {
+        var sql = 'select * from sify_darc_modules_query where module_name="' + name + '"  order by sequence asc';
+        sour_con.query(sql, function(err, result, fields) {
+            if (err || !result.length > 0) {
+                rj(new Error('Module Name is not valide. Module Name data not exist. '));
+                log.info('Module Name is not valide. Module Name data not exist');
+                return false;
+            } else {
+                if (result.length > 0)
+                    rs(result);
+            }
+        });
     });
-  });
+}
+
+
+async function source_conn(sour_con, dest_con) {
+    return new Promise((rs, rj) => {
+        sour_con.connect(function(err, result) {
+            if (err != null) {
+                rj(new Error('Source database not connected !.. Please make sure source database configuration valid.'));
+                return false;
+              
+            } else {
+               log.info('Source database connected successfully.');
+                dest_con.connect(function(err) {
+                    if (err != null) {
+                        rj(new Error('Destination database not connected !.. Please make sure destination Details '));
+                        return false;
+                     
+                    } else {
+                        log.info('Destination database connected successfully.');
+                        rs(true);
+                    }
+                });
+            }
+        });
+    });
 }
 
 
 
+app.listen(3000, () => {});
 
-
-  app.listen(3000, () => {  });
-
-module.exports =app;
+module.exports = app;
--- forumnxt_archive.js
+++ forumnxt_archive.js
@@ -3,17 +3,18 @@ var insert_columns;
 var tbl_columns;
 
 async function do_archive(sour_con, dest_con, create_table, module_name, sour_db, dest_db, sour_host, dest_host, sour_port, dest_port, sour_table, dest_table, sel_query, del_query, sequence) {
-    return new Promise((rs, rj) => {
+    return new Promise((resolve, reject) => {
         return table_exists(dest_con, dest_db, dest_table)
             .then(stat => {
-                if (stat) {
+                if (stat) 
+                {
                     return import_table_data(sour_con, dest_con, sour_db, dest_db, sour_host, dest_host, sour_port, dest_port, sour_table, dest_table, sel_query)
-                        .then(rslt => {
-                            if (rslt) {
-                                rs(delete_data(sour_con, del_query, sequence, module_name, sour_db));
+                        .then(result => {
+                            if (result) {
+                                resolve(delete_data(sour_con, del_query, sequence, module_name, sour_db));
                             } else {
-                                rj(new Error(`No data available for selected Select Module Name...`));
-                                return false;
+                                reject(new Error(`No data available for selected Select Module Name...`));
+                                //return false;
                             }
                         })
                 } else if (create_table) {
@@ -24,12 +25,12 @@ async function do_archive(sour_con, dest_con, create_table, module_name, sour_db
                                     var p$;
                                     if (stat) {
                                         return import_table_data(sour_con, dest_con, sour_db, dest_db, sour_host, dest_host, sour_port, dest_port, sour_table, dest_table, sel_query)
-                                            .then(rslt => {
+                                            .then(resolvelt => {
                                                 var p$;
-                                                if (rslt) {
-                                                    rs(delete_data(sour_con, del_query, sequence, module_name, sour_db));
+                                                if (resolvelt) {
+                                                    resolve(delete_data(sour_con, del_query, sequence, module_name, sour_db));
                                                 } else {
-                                                    rj(new Error(`No data available for selected Select Module Name...`));
+                                                    reject(new Error(`No data available for selected Select Module Name...`));
                                                     return false;
                                                 }
                                             })
@@ -38,76 +39,68 @@ async function do_archive(sour_con, dest_con, create_table, module_name, sour_db
                         });
                 } else {
                     log.error('Table dose not exists in destination database . Also create table not enabled.');
-                    // p$=Promise.reject(new Error('Table dose not exists in destination database . Also create table not enabled.'));
-                    rj(new Error('Table dose not exists in destination database . Also create table not enabled.'));
+                    reject(new Error('Table dose not exists in destination database . Also create table not enabled.'));
                     return false;
                 }
             });
-        // }
-        // else{
-        //      log.error('Table dose not exists in destination database . Also create table not enabled.');
-        //     p$=Promise.reject(new Error('Table dose not exists in destination database . Also create table not enabled.'));
-        //     return false;
-        // }
-        // });
-    });
+     });
 }
 
 
 
 async function table_exists(conn, dest_db, name) {
     var sql = `SELECT count(*) FROM information_schema.tables WHERE table_schema = '${dest_db}' AND table_name = '${name}'`;
-    return new Promise((rs, rj) => {
+    return new Promise((resolve, reject) => {
         conn.query(sql, function(err, results, fields) {
             if (err) {
-                rj(new Error('Error while getting info about table.'));
+                reject(new Error('Error while getting info about table.'));
                 return false;
             }
             if (results[0]['count(*)'] == 0) {
-                rs(false);
+                resolve(false);
             } else {
-                rs(true);
+                resolve(true);
             }
         });
     });
 }
 async function get_table_schema(conn, name, dest_table) {
-    return new Promise((rs, rj) => {
+    return new Promise((resolve, reject) => {
         conn.query(`SHOW CREATE TABLE ${name}`, function(err, results, fields) {
             if (err) {
-                rj(new Error(`Error while getting the "${name}" schema.`));
+                reject(new Error(`Error while getting the "${name}" schema.`));
                 return false;
             }
             var schema = (results.length > 0 && results[0] && results[0]['Create Table']) || null;
             if (!schema) {
-                rj(new Error(`Error while getting the "${name}" schema.`));
+                reject(new Error(`Error while getting the "${name}" schema.`));
                 return false;
             }
-            rs(schema = schema.replace(name, dest_table));
+            resolve(schema = schema.replace(name, dest_table));
         });
     });
 }
 
 async function get_table_columns(conn, sour_db, sour_table) {
-    return new Promise((rs, rj) => {
+    return new Promise((resolve, reject) => {
         var sql = 'SELECT COLUMN_NAME  FROM information_schema.columns WHERE table_name ="' + sour_table + '" AND table_schema ="' + sour_db + '"   ORDER BY ORDINAL_POSITION';
-        conn.query(sql, function(err, rslt, fields) {
-            if (err || !rslt) {
-                rj(new Error(`Error while get columns names from source table...` + err.message));
+        conn.query(sql, function(err, resolvelt, fields) {
+            if (err || !resolvelt) {
+                reject(new Error(`Error while get columns names from source table...` + err.message));
                 return false;
             } else {
 
-                tbl_columns = rslt;
-                for (var i = 0; i < rslt.length; i++) {
-                    if (i < rslt.length - 1) {
+                tbl_columns = resolvelt;
+                for (var i = 0; i < resolvelt.length; i++) {
+                    if (i < resolvelt.length - 1) {
                         if (i == 0)
-                            insert_columns = rslt[i]['COLUMN_NAME'] + ',';
+                            insert_columns = resolvelt[i]['COLUMN_NAME'] + ',';
                         else
-                            insert_columns += rslt[i]['COLUMN_NAME'] + ',';
+                            insert_columns += resolvelt[i]['COLUMN_NAME'] + ',';
                     } else
-                        insert_columns += rslt[i]['COLUMN_NAME'];
+                        insert_columns += resolvelt[i]['COLUMN_NAME'];
                 }
-                rs(true);
+                resolve(true);
             }
         });
     });
@@ -115,17 +108,17 @@ async function get_table_columns(conn, sour_db, sour_table) {
 
 
 async function import_table_schema(conn, schema) {
-    return new Promise((rs, rj) => {
+    return new Promise((resolve, reject) => {
         conn.query(schema, function(err, results, fields) {
             if (err || !results) {
                 if (err.message.indexOf('ER_TABLE_EXISTS_ERROR:') >= 0) {
-                    rs(true);
+                    resolve(true);
                 } else {
-                    rj(new Error(`Error while creating new table. ` + err.message));
+                    reject(new Error(`Error while creating new table. ` + err.message));
                     return false;
                 }
             } else {
-                rs(true);
+                resolve(true);
             }
         });
     });
@@ -135,15 +128,15 @@ async function import_table_schema(conn, schema) {
 function import_table_data(sour_con, dest_con, sour_db, dest_db, sour_host, dest_host, sour_port, dest_port, sour_table, dest_table, sel_query) {
     /*  Source and destination host and port is different */
     if (sour_host != dest_host && sour_port != dest_port) {
-        return new Promise((rs, rject) => {          
+        return new Promise((resolve, rejectect) => {          
             sour_con.query(sel_query, function(err, result, fields) {
                 if (err || !result) {
-                    rject(new Error(`Error while get data from source table..`));
+                    rejectect(new Error(`Error while get data from source table..`));
                     return false;
                 } else {
                     return get_table_columns(sour_con, sour_db, sour_table)
-                        .then(rslt => {
-                            if (rslt) {
+                        .then(resolvelt => {
+                            if (resolvelt) {
                                 if (result && result.length > 0) {
                                     var sql = "INSERT INTO " + dest_table + " (" + insert_columns + ") VALUES ?";
                                     var values = [];
@@ -156,15 +149,15 @@ function import_table_data(sour_con, dest_con, sour_db, dest_db, sour_host, dest
                                     }
                                     dest_con.query(sql, [values], function(err, result) {
                                         if (err || !result) {
-                                            rject(new Error(`Error while import data to destination table.` + err.message));
+                                            rejectect(new Error(`Error while import data to destination table.` + err.message));
                                             return false;
                                         } else {
                                             log.info('Number of records inserted: ' + result.affectedRows);
-                                            rs(true);
+                                            resolve(true);
                                         }
                                     });
                                 } else {
-                                    rject(new Error(`No data available for selected Select Module Name...`));
+                                    rejectect(new Error(`No data available for selected Select Module Name...`));
                                     return false;
                                 }
                             }
@@ -175,18 +168,17 @@ function import_table_data(sour_con, dest_con, sour_db, dest_db, sour_host, dest
     }
     /* IF Source and Destination host and port are same.*/
     else {
-        return new Promise((rs, rj) => {
+        return new Promise((resolve, reject) => {
             var tbl = sour_db + "." + sour_table;
             var query = replaceAll(sel_query, sour_table, tbl);
-            //  var query= replaceAll(query,";","");
             var sql = "INSERT INTO " + dest_db + "." + dest_table + " (" + query + ") ";
-             console.log(sql);
             dest_con.query(sql, function(err, result, fields) {
-                if (err || !result) {
-                    rj(new Error(`Error while get data from source table..`+ err.message));
-                    return false;
+                 if (err) {
+                    console.log(err.message);
+                    console.log(result);
+                    reject(new Error(`Error while get data from source table..`));
                 } else {
-                    rs(true);
+                    resolve(true);
                 }
 
             });
@@ -196,21 +188,18 @@ function import_table_data(sour_con, dest_con, sour_db, dest_db, sour_host, dest
 
 
 function delete_data(conn, del_query, sequence, module_name, sour_db) {
-    return new Promise((rs, rj) => {
+    return new Promise((resolve, reject) => {
             conn.query(del_query, function(err, results, fields) {
                 if (err || !results) {
-                    rj(new Error(`Error while delete table data.`));
+                    reject(new Error(`Error while delete table data.`));
                     return false;
                 } else {
                     log.info('Number of records deleted: ' + results.affectedRows);
                     log.log_entry(conn, sequence, module_name, '1', sour_db);
-                    rs(true);
+                    resolve(true);
                 }
             });
-        })
-        .then(sat => {
-            return sat;
-        });
+        });        
 }
 
 function replaceAll(originalString, find, replace) {
